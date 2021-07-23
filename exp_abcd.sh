1bf7136e2e7a504d97f280dcf0ecb1af5480f3ee
Oscar Liu <1026oscar1@gmail.com>
2021-07-23 21:56:42 +0800

    upload bash for running abcd experiment

---
+++ exp_abcd.sh
@@ -0,0 +1,4 @@
+python main.py --expset a --T 200 --plot_rate 0.5
+python main.py --expset b --T 200 --plot_rate 0.5
+python main.py --expset c --T 200 --plot_rate 0.5
+python main.py --expset d --T 200 --plot_rate 0.5
